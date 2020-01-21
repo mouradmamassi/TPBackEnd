eb7bea161bd5cbf858ea8c64ca9a540a6613d34f
Mourad MAMASSI <mourad.mamassi@gmail.com>
2020-01-21 20:48:13 +0100

    creation de projet

---
+++ src/main/java/com/tp/backend/TPBackEnd/TpBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.tp.backend.TPBackEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tp/backend/TPBackEnd/TpBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tp.backend.TPBackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
